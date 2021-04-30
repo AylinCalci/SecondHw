8ab524f9b0974ac4834f19547475582656f6e862
AylinCalci <aylincalci@hotmail.com>
2021-05-01 02:28:25 +0300

    Hw2

---
+++ assignment2/src/assignmentt/Category.java
@@ -0,0 +1,18 @@
+package assignmentt;
+
+public class Category {
+
+	
+	public Category(int CategoryId, String Categoryname, int CourseNumber) {
+				this.CategoryId = CategoryId;
+				this.Categoryname = Categoryname; 
+				this.CourseNumber = CourseNumber;
+			}
+			int CategoryId;
+			String Categoryname;
+			int CourseNumber;
+		}
+
+	
+
+
---
+++ assignment2/src/assignmentt/Course.java
@@ -0,0 +1,18 @@
+package assignmentt;
+
+public class Course {
+
+	public Course(int CourseId, String CourseName, String CourseTeacher ) {
+				this.CourseId = CourseId;
+				this.CourseName = CourseName; 
+				this.CourseTeacher = CourseTeacher;
+				
+			}
+			int CourseId;
+			String CourseName;
+			String CourseTeacher;
+			
+		
+
+	}
+
